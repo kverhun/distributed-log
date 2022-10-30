2d40330adafb3833a725eade88a3fa44122126a4
Kostiantyn Verhun <k.verhun@gmail.com>
2022-10-30 13:07:53 +0200

    Responses

--- src/master-app/master_app.cpp
+++ src/master-app/master_app.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include <Server.h>
+
+std::vector<std::string> InMemoryMessageStorage;
+
+std::string MessageListResponseStr(const std::vector<std::string>& messages) {
+    std::string res = "[";
+    if (!messages.empty()) {
+        res += messages.front();
+    }
+    for (auto i = 1u; i < messages.size(); ++i) {
+        res += ", ";
+        res += messages[i];
+    }
+    res += "]\n";
+    return res;
+}
 
 int main() {
     std::atomic_bool stopped = false;
@@ -8,11 +25,14 @@ int main() {
 
     server.SetRequestHandlerPost([](const std::string& str){
         std::cout << "Post message: " << str << "\n";
-        return "POST accepted";
+
+        InMemoryMessageStorage.push_back(str);
+
+        return std::string("Message \"" + str + "\" accepted\n");
     });
     server.SetRequestHandlerGet([](const std::string& str){
         std::cout << "Get message: " << str << "\n";
-        return "GET accepted";
+        return MessageListResponseStr(InMemoryMessageStorage);
     });
 
     server.Listen(stopped);
